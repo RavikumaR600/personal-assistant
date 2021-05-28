fefbfbb5c5386bdfd47fd22a3a00776059033084
unknown <67065363+RavikumaR600@users.noreply.github.com>
2021-05-28 18:43:21 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import wolframalpha
+import wikipedia
+from tkinter import *
+import speech_recognition as sr
+
+while True:
+	r = sr.Recognizer()
+
+	with sr.Microphone() as source:
+		print("Listening.....")
+		audio = r.listen(source)
+		try:
+			print("Regognizing....")
+			text = r.recognize_google(audio)
+			print('You said: ' + text)
+			if text == "stop":
+				print("Program will exit.")
+				break
+			else:
+				window = Tk()
+				window.geometry("700x500")
+				try:
+					app_id = "enter your app id here"
+					client = wolframalpha.Client(app_id)
+					res = client.query(text)
+					answer = next(res.results).text
+					print("Answer from WolframAlpha:")
+					print(answer)
+					label1 = Label(window, justify=LEFT, wraplength=650, compound=CENTER, padx=10, text=answer, font='times 15 bold')
+					label1.pack()
+					window.after(5000, lambda: window.destroy())
+					mainloop()
+				except Exception as e:
+					print("No results from WolframAlpha. Trying wikipedia...")
+					answer = wikipedia.summary(text)
+					print("Answer from Wikipedia:")
+					print(answer)
+					label1 = Label(window, justify=LEFT, wraplength=650, compound=CENTER, padx=10, text=answer, font='times 15 bold')
+					label1.pack()
+					# window.after(500000, lambda: window.destroy())
+					mainloop()
+		except Exception as e:
+			print(e)
+			answer = "Sorry,did'nt hear you"
+			print(answer)
